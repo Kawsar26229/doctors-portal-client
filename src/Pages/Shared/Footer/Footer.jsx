7981103e2e8687538896c86314b71072e76d83d5
Kawsar26 <knkawsar26229@gmail.com>
2023-02-16 23:26:23 +0600

    footer done

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import footer from '../../../assets/images/footer.png';
 
 const Footer = () => {
   return (
-    <div>
-      <h2>This is footer</h2>
-    </div>
+    <footer className="p-10 mt-12">
+      <div
+        className="footer"
+        style={{ backgroundImage: `url(${footer})`, backgroundSize: 'cover' }}
+      >
+        <div>
+          <span className="footer-title">Services</span>
+          <Link to="/" className="link link-hover">
+            Emergency Checkup
+          </Link>
+          <Link to="/" className="link link-hover">
+            Monthly Checkup
+          </Link>
+          <Link to="/" className="link link-hover">
+            Weekly Checkup
+          </Link>
+          <Link to="/" className="link link-hover">
+            Deep Checkup
+          </Link>
+        </div>
+        <div>
+          <span className="footer-title">Oral Health</span>
+          <Link to="/" className="link link-hover">
+            Fluoride Treatment
+          </Link>
+          <Link to="/" className="link link-hover">
+            Cavity Treatment
+          </Link>
+          <Link to="/" className="link link-hover">
+            Teath Whitening
+          </Link>
+        </div>
+        <div>
+          <span className="footer-title">Our Address</span>
+          <p>Mirpur-10, Dhaka-1216</p>
+        </div>
+      </div>
+      <div className="text-center mt-16">
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Doctors
+          Portal Community
+        </p>
+      </div>
+    </footer>
   );
 };
 
